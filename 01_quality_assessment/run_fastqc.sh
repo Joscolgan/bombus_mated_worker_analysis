bf50aaa03dde5bfcb73a8d86d66708c0ecfcca4a
Joe Colgan <tcolgan@tcd.ie>
2022-12-14 23:17:35 +0100

    Update run_fastqc.sh

--- 01_quality_assessment/run_fastqc.sh
+++ 01_quality_assessment/run_fastqc.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+########################################################################
+##
+## Author: Joe Colgan (joscolgan)                 Name: run_fastqc.sh
+##
+## Purpose:
+## This script takes pairs of compressed fastq files and performs 
+## a basic quality assessment analysis using fastqc. 
+## The script produces a html file as output summarises the results
+## of the assessment.
+##
+########################################################################
 
+## Create a results output directory:
 mkdir results
 
+## Run fastqc for all compressed files in input directory and output
+## to newly created results directory:
 fastqc -t 10 input/*gz -o results
