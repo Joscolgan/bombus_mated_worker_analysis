d1705e816c036f56cb8b8d276086e3a83ef176a9
Joe Colgan <tcolgan@tcd.ie>
2022-12-14 23:22:56 +0100

    Update make_star_index.sh

--- 03_transcript_quantification/make_star_index.sh
+++ 03_transcript_quantification/make_star_index.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+########################################################################
+##
+## Author: Joe Colgan (joscolgan)               Name: make_star_index.sh
+##
+## Purpose:
+## This script generates a genome index using STAR, a common short read
+## aligner for RNA-seq datasets. This script takes three input arguments
+## from the command line to generate the genome index. The arguments are:
+## 1) A reference genome assembly in FASTA format.
+## 2) A corresponding GFF for the reference genome assembly.  
+## 3) The overhang of the input sequences.
+## The script outputs a folder containing genome index.
+########################################################################
 
 ## Take inputs from the command line:
 input_fasta=$1
@@ -13,7 +26,7 @@ if [ $# -eq 0 ]
 
 fi
 
-## Create STAR indices
+## Create STAR indices:
 STAR --runThreadN 2 \
      --runMode genomeGenerate \
      --genomeDir ./database/ \
